f50a9f964ec5d242282dda29a30362bf3f341ed6
biscotty666 <biscotty666@gmail.com>
2023-01-27 09:38:14 -0700

    Happy Faces Part 3

--- src/routes/happyface/+page.svelte
+++ src/routes/happyface/+page.svelte
@@ -1,34 +1,45 @@
 <script>
-	const width = 960
-  const height = 500
-	const centerX = width / 2
-	const centerY = height / 2
-	const strokeWidth = 10
-	const eyeOffsetX = 90
-	const eyeOffsetY = 100
-	const eyeRadius = 50
+	import { arc } from "d3";
+
+	const width = 960;
+	const height = 500;
+	const centerX = width / 2;
+	const centerY = height / 2;
+	const strokeWidth = 10;
+	const eyeOffsetX = 90;
+	const eyeOffsetY = 100;
+	const eyeRadius = 50;
+	const mouthWidth = 20
+	const mouthRadius = 140
+
+	const mouthArc = arc()
+		.innerRadius(mouthRadius)
+		.outerRadius(mouthRadius + mouthWidth)
+		.startAngle(Math.PI / 2)
+		.endAngle(Math.PI * 3 / 2);
 </script>
 
 <div class="container">
-	<svg width={width} height={height}>
-		<circle 
-			cx={centerX} 
-			cy={centerY} 
-			r={centerY - strokeWidth} 
-			fill="yellow"
-			stroke="black"
-			stroke-width={strokeWidth}
-		/>
-		<circle 
-			cx={centerX - eyeOffsetX} 
-			cy={centerY - eyeOffsetY} 
-			r={eyeRadius} 
-		/>
-		<circle 
-			cx={centerX + eyeOffsetX}  
-			cy={centerY - eyeOffsetY} 
-			r={eyeRadius} 
-		/>
+	<svg {width} {height}>
+		<g transform={`translate(${centerX},${centerY})`}>
+			<circle
+				r={centerY - strokeWidth}
+				fill="yellow"
+				stroke="black"
+				stroke-width={strokeWidth}
+			/>
+			<circle
+				cx={-eyeOffsetX}
+				cy={-eyeOffsetY}
+				r={eyeRadius}
+			/>
+			<circle
+				cx={eyeOffsetX}
+				cy={-eyeOffsetY}
+				r={eyeRadius}
+			/>
+			<path d={mouthArc()} />
+		</g>
 	</svg>
 </div>
 
