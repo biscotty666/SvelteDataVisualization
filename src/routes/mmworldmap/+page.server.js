15a593f4ace1f613f64837e222b44df7df33ec40
biscotty666 <biscotty666@gmail.com>
2023-01-29 12:12:02 -0700

    Try to fix build error on vericel again

--- src/routes/mmworldmap/+page.server.js
+++ src/routes/mmworldmap/+page.server.js
@@ -1,5 +1,6 @@
 import { json } from "d3"
-import { feature, mesh } from "topojson"
+import { mesh } from "topojson-client"
+import { feature } from "topojson-client"
 
 const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json'
 
